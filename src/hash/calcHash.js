9ad0bd18a6b54b10dc396c5ddd61c4ace827453b
Александр Иванов <avi@digt.ru>
2022-12-05 23:22:22 +0300

    implement: hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { access, readFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import crypto from 'node:crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const folderName = 'files';
+const fileName = 'fileToCalculateHashFor.txt';
+const pathToFile = join(__dirname, folderName, fileName);
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    await access(pathToFile);
+    const file = await readFile(pathToFile);
+    const hashSum = crypto.createHash('sha256');
+    hashSum.update(file);
+    const hex = hashSum.digest('hex');
+    console.log(hex);
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
 await calculateHash();

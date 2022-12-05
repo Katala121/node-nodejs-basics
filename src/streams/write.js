acf88ab33a0774ef6add2109662a73667e13762f
Александр Иванов <avi@digt.ru>
2022-12-06 00:46:17 +0300

    implement: read and write of streams task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import { access } from 'node:fs/promises';
+import { createWriteStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { stdin } from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const folderName = 'files';
+const fileName = 'fileToWrite.txt';
+const pathToFile = join(__dirname, folderName, fileName);
+
 const write = async () => {
-    // Write your code here 
+  try {
+    await access(pathToFile);
+    const writeStream = createWriteStream(pathToFile);
+    stdin.pipe(writeStream);
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
 await write();
